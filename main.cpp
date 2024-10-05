1458a8465fa2dbb9b5250c945ccbcdfb1ada2178
JZwlth <wkjazy87@gmail.com>
2024-10-04 18:17:47 -0700

    update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 int main() {
 
     // add any code here, sort of a 'hello world plus' code block
+    cout << "Hello, World!" << endl;
+
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    int sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
 
     return 0;
 }
